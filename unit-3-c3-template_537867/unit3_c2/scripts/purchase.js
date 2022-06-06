623f8afc8a168f0aba350aed1599a2319f12e032
Guddu Kumar Choudhary <guddukumar270196@gmail.com>
2022-06-07 00:16:50 +0530

    edit

--- unit-3-c3-template_537867/unit3_c2/scripts/purchase.js
+++ unit-3-c3-template_537867/unit3_c2/scripts/purchase.js
@@ -1,27 +1,29 @@
     
 
      let purchased_vouchers =JSON.parse(localStorage.getItem("purchase"))
-     console.log(purchased_vouchers)
-let container=document.getElementById("purchased_vouchers")
-purchased_vouchers.forEach(function(ele){
-let div=document.createElement("div")
+     //console.log(purchased_vouchers)
+    let container=document.getElementById("purchased_vouchers")
 
-let img=document.createElement("img")
-    img.src=ele.image
+        purchased_vouchers.forEach(function(ele){
+         let div=document.createElement("div")
 
-    let h1=document.createElement("h3")
-    h1.innerText=ele.name
+         let img=document.createElement("img")
+          img.src=ele.image
 
-    let h2=document.createElement("h3")
-    h2.innerText=ele.price
+          let h1=document.createElement("h3")
+            h1.innerText=ele.name
 
-    div.append(img,h1,h2)
-    container.append(div)
-})
- let t_sum=document.getElementById("wallet_balance")
-  let sum=0;
-  for(let i=0;i< purchased_vouchers.length-1;i++){
-    sum+= purchased_vouchers[i].price
-  }
-  console.log(sum)
-  t_sum.append(sum)
+          let h2=document.createElement("h3")
+          h2.innerText=ele.price
+
+          div.append(img,h1,h2)
+           container.append(div)
+       })
+         let t_sum=document.getElementById("wallet_balance")
+       let sum=0;
+      for(let i=0;i< purchased_vouchers.length;i++){
+        sum+= purchased_vouchers[i].price;
+        //console.log(sum)
+      }
+      console.log(sum)
+      t_sum.append(sum)
--- unit-3-c3-template_537867/unit3_c2/scripts/voucher.js
+++ unit-3-c3-template_537867/unit3_c2/scripts/voucher.js
@@ -1,7 +1,7 @@
      
 
       let total=document.getElementById("wallet_balance")
-    total.innerHTML=null
+   // total.innerHTML=null
           let details=JSON.parse(localStorage.getItem("user"))
         
           //console.log(details)
@@ -9,7 +9,7 @@
 
 
 
-  async function  getData(){
+   async function  getData(){
    let url=" https://masai-vouchers-api.herokuapp.com/api/vouchers"
      try{
      let res=await fetch(url)
@@ -43,25 +43,37 @@
         btn.addEventListener("click",function(){
           // console.log(details)
           // console.log(ele.price)
+         
+          
           if(ele.price<=details.amount){
             alert("Hurray! purchase successful")
             // details.amount =details.amount-ele.price
             // console.log(details.amount)
-
+            let arr=JSON.parse(localStorage.getItem("purchase"))||[]
+            arr.push(ele)
+            localStorage.setItem("purchase",JSON.stringify(arr))
+            // window.location.reload()
           }
           else{
             alert("Sorry! insufficient balance")
           }
-          details.amount =details.amount-ele.price
-            console.log(details.amount)
-            total.innerHTML=null
-            total.append(details.amount)
-            
 
-          let arr=JSON.parse(localStorage.getItem("purchase"))||[]
-          arr.push(ele)
-          localStorage.setItem("purchase",JSON.stringify(arr))
-          // window.location.reload()
+          //total.innerHTML=null
+            
+          
+         details.amount =details.amount-ele.price;
+
+           // console.log(details.amount)
+           if(details.amount<=0){
+            total.innerHTML=null
+            total.innerHTML="00"
+           }
+           
+           else{
+            total.innerHTML=null
+            
+           total.append(details.amount) 
+           }
         })
      
         div.append(img,h1,h2,btn)
